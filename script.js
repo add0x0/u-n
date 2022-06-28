dcecb657cbc411b36aca048058203e9bdc227bc6
add0x0 <woksoba@pm.me>
2022-06-28 11:18:27 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// const a = document.querySelector('#logo-switch')
+
+// document.addEventListener('click', function(e) {
+//     if(!event.target.closest('#logo-active-area')) return;
+
+//     switch (a.checked) {
+//         case true:
+//             a.checked = false;
+//             break;
+        
+//         case false:
+//             a.checked = true;
+//             break;
+
+//         default:
+//             break;
+//     }
+// })
